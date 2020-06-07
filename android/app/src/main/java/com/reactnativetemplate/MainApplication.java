a1cd0a8385d3fc242f0e4325f8436950a966b713
Juliette Rapala <juliette@eventric.com>
2020-06-07 14:06:50 -0500

    Init new project

--- android/app/src/main/java/com/reactnativetemplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativetemplate/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativetemplate;
+package com.reactnativenews;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativetemplate.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.reactnativenews.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/ReactNativeNewsTests/ReactNativeNewsTests.m
+++ ios/ReactNativeNewsTests/ReactNativeNewsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeTemplateTests : XCTestCase
+@interface ReactNativeNewsTests : XCTestCase
 
 @end
 
-@implementation ReactNativeTemplateTests
+@implementation ReactNativeNewsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
