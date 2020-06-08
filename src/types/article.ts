e601d9b4085afb3b1102b88a26c98f38264d490e
Juliette Rapala <juliette@eventric.com>
2020-06-08 06:35:52 -0500

    Add Article type

---
+++ src/types/article.ts
@@ -0,0 +1,8 @@
+export interface Article {
+	author: string | null
+	title: string | null
+	description: string | null
+	url: string | null
+	urlToImage: string | null
+	publishedAt: string | null
+}
