37f68965dc4474a533ffe39039c3fa0c878dcd75
Juliette Rapala <juliette@eventric.com>
2020-06-08 06:34:21 -0500

    Adjust ts and stylelint configs to accept react-native css properties

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,4 +4,24 @@ module.exports = {
 		"stylelint-config-recommended",
 		"stylelint-config-styled-components",
 	],
+	rules: {
+		"property-no-unknown": [
+			true,
+			{
+				ignoreProperties: [
+					"shadow-color",
+					"shadow-opacity",
+					"shadow-offset",
+					"shadow-radius",
+					"padding-horizontal",
+					"padding-vertical",
+					"margin-vertical",
+					"margin-horizontal",
+					"tint-color",
+					"aspect-ratio",
+					"elevation",
+				],
+			},
+		],
+	},
 }
