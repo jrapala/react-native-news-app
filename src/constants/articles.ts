94e7ce9efb07cbf51820a721eeead39491efe93a
Juliette Rapala <juliette@eventric.com>
2020-06-08 06:34:51 -0500

    Add mock data

---
+++ src/constants/articles.ts
@@ -0,0 +1,28 @@
+/* eslint-disable sort-keys */
+export const articles = [
+	{
+		author: "Rob Picheta, CNN",
+		title:
+			"After months of lockdown, people across the world head outside to protest against racism - CNN",
+		description:
+			// eslint-disable-next-line quotes
+			'In London, Rome, Madrid, Warsaw, Sydney, Hong Kong and countless other cities, protesters are packing streets that have laid deserted for months to tell governments around the world that "Black Lives Matter."',
+		url:
+			"https://www.cnn.com/2020/06/07/world/global-floyd-protests-weekend-intl/index.html",
+		urlToImage:
+			"https://cdn.cnn.com/cnnnext/dam/assets/200607162156-brisbane-aus-protest-0706-super-tease.jpg",
+		publishedAt: "2020-06-07T16:12:41Z",
+	},
+	{
+		author: null,
+		title:
+			"Barr says Trump never demanded 10,000 active-duty troops - POLITICO",
+		description:
+			"The attorney general said the president never went that far.",
+		url:
+			"https://www.politico.com/news/2020/06/07/barr-trump-active-duty-troops-305649",
+		urlToImage:
+			"https://static.politico.com/94/3e/a475d6b24f60973796c399178035/gettyimages-barrjpg.jpg",
+		publishedAt: "2020-06-07T16:08:32Z",
+	},
+]
