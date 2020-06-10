13cdd90cbb69d2a051a4221146dc4522aef5d910
Juliette Rapala <juliette@eventric.com>
2020-06-10 14:41:40 -0500

    Move Headlines to new component and fetch data from graphql

--- src/views/TopHeadlines/index.tsx
+++ src/views/TopHeadlines/index.tsx
@@ -1,63 +1,15 @@
-import React, { useEffect, useState } from "react"
-import {
-	ActivityIndicator,
-	FlatList,
-	SafeAreaView,
-	View,
-	Text,
-} from "react-native"
+import React from "react"
+import { SafeAreaView, View, Text } from "react-native"
 import styled from "styled-components/native"
 
-import { Article } from "../../types/article"
-import ArticleCard from "../../components/ArticleCard"
-import { fetchHeadlines } from "../../utils/fetchHeadlines"
+import Headlines from "../../components/Headlines"
 
 const TopHeadlines: React.FC = () => {
-	const [articles, setArticles] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
-	const [isRefreshing, setIsRefreshing] = useState(false)
-
-	const handleLoad = () => {
-		setIsLoading(true)
-		fetchHeadlines()
-			.then(headlines => setArticles(headlines))
-			.then(() => {
-				setIsLoading(false)
-			})
-	}
-
-	const handleRefresh = () => {
-		setIsRefreshing(true)
-		fetchHeadlines()
-			.then(headlines => setArticles(headlines))
-			.then(() => {
-				setIsRefreshing(false)
-			})
-	}
-
-	useEffect(() => {
-		handleLoad()
-	}, [])
-
 	return (
 		<StyledSafeAreaView>
 			<Container>
 				<H1>Top Headlines</H1>
-				{isLoading ? (
-					<Spinner />
-				) : (
-					<Articles>
-						<FlatList
-							data={articles}
-							renderItem={({ item }: { item: Article }) => (
-								<ArticleCard article={item} />
-							)}
-							keyExtractor={(item): string => item.title}
-							refreshing={isRefreshing}
-							onRefresh={handleRefresh}
-						/>
-					</Articles>
-				)}
+				<Headlines />
 			</Container>
 		</StyledSafeAreaView>
 	)
@@ -73,13 +25,6 @@ const Container = styled(View)`
 	flex: 1;
 	margin-top: 5%;
 	width: 80%;
-`
-const Spinner = styled(ActivityIndicator)`
-	flex: 1;
-`
-const Articles = styled(View)`
-	align-items: center;
-	margin-top: 5%;
 `
 
 const H1 = styled(Text)`
