bc3aa5895b61672f6a8c5d4edcb6ac787ccbbf92
Juliette Rapala <juliette@eventric.com>
2020-06-10 14:40:27 -0500

    Install graphql libaries and create Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "react-native-gesture-handler"
 import React, { useContext, useState } from "react"
-import { StatusBar } from "react-native"
+import { StatusBar, YellowBox } from "react-native"
 import { ThemeProvider, ThemeContext } from "styled-components/native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import { ApolloProvider } from "@apollo/react-hooks"
 
 import { RootStackParamList } from "./types/navigation"
 import { iOSDarkTheme } from "./styles"
 import TopHeadlines from "./views/TopHeadlines"
 import HeadlineDetails from "./views/HeadlineDetails"
 import { FavoritesProvider } from "./utils/store"
+import { client } from "./graphql/client"
 
 const Stack = createStackNavigator<RootStackParamList>()
 
@@ -40,13 +42,19 @@ const StackNavigator: React.FC = () => {
 const App: React.FC = () => {
 	const [theme] = useState(iOSDarkTheme)
 
+	// Ignore warning emitted from apollo-link-rest
+	// Debug for another time..
+	YellowBox.ignoreWarnings(["Require cycle:"])
+
 	return (
 		<NavigationContainer>
 			<ThemeProvider theme={theme}>
 				<StatusBar barStyle="dark-content" />
-				<FavoritesProvider>
-					<StackNavigator />
-				</FavoritesProvider>
+				<ApolloProvider client={client}>
+					<FavoritesProvider>
+						<StackNavigator />
+					</FavoritesProvider>
+				</ApolloProvider>
 			</ThemeProvider>
 		</NavigationContainer>
 	)
