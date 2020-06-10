4d58f2279dcec3bafc7a4e5f92bff091f27cb5d5
Juliette Rapala <juliette@eventric.com>
2020-06-10 16:52:31 -0500

    Add useScreenDimensions hook

---
+++ src/hooks/useScreenDimensions.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import { Dimensions, ScaledSize } from "react-native"
+
+interface ScreenDimensions extends ScaledSize {
+	isLandscape: boolean
+}
+
+const useScreenDimensions = (): ScreenDimensions => {
+	const [screenData, setScreenData] = useState(Dimensions.get("screen"))
+
+	useEffect(() => {
+		const handleChange = (result: {
+			window: ScaledSize
+			screen: ScaledSize
+		}): void => {
+			setScreenData(result.screen)
+		}
+
+		Dimensions.addEventListener("change", handleChange)
+
+		return () => Dimensions.removeEventListener("change", handleChange)
+	})
+
+	return {
+		...screenData,
+		isLandscape: screenData.width > screenData.height,
+	}
+}
+
+export default useScreenDimensions
