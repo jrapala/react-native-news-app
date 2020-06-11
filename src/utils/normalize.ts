3783384098bd9aa292817ac9c88f1ff9a14dc663
Juliette Rapala <juliette@eventric.com>
2020-06-10 19:37:07 -0500

    Add iPad support and improve font sizes on tablets

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -2,11 +2,25 @@ import { Dimensions, Platform, PixelRatio } from "react-native"
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window")
 
-// Based on iPhone 5S's scale
+// Based on iPhone 5s's scale
 const scale = SCREEN_WIDTH / 320
 
+const checkIfTablet = () => {
+	const pixelDensity = PixelRatio.get()
+	const adjustedWidth = SCREEN_WIDTH * pixelDensity
+	const adjustedHeight = SCREEN_WIDTH * pixelDensity
+	if (pixelDensity < 2 && (adjustedWidth >= 1000 || adjustedHeight >= 1000)) {
+		return true
+	} else {
+		return (
+			pixelDensity === 2 && (adjustedWidth >= 1920 || adjustedHeight >= 1920)
+		)
+	}
+}
+
 export default function normalize(size: number) {
-	const newSize = size * scale
+	const isTablet = checkIfTablet()
+	const newSize = isTablet ? (size * scale) / 2 : size * scale
 	if (Platform.OS === "ios") {
 		return Math.round(PixelRatio.roundToNearestPixel(newSize))
 	} else {
